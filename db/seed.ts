1985bf3631d604c03c9bd509b4bfa3ff85a11978
ciandre <a.stanisic.codealkonost@gmail.com>
2025-02-05 16:25:40 +0100

    Add sample user data and bcrypt for password hashing; update seed script

--- db/seed.ts
+++ db/seed.ts
@@ -3,7 +3,13 @@ import { prisma } from "@/db/prisma";
 
 async function main() {
   await prisma.product.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 }
 
 main();
