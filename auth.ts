092f1f0940075932047682d58a1366784d1fed7d
ciandre <a.stanisic.codealkonost@gmail.com>
2025-02-10 18:37:37 +0100

    Add session cart ID handling in authorized callback to manage cart cookies

--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import { prisma } from "./db/prisma";
 import CredetialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -70,8 +72,32 @@ export const config = {
           });
         }
       }
-      console.log("TOKEN", token);
       return token;
+    },
+    authorized({ request, auth }: any) {
+      // Check for cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate cart cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create a new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set the newly generated sessionCartId in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        // Return the response with the sessionCartId set
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
